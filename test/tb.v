88323d235d7f92ecdca1bc7c08ef752b05647db1
SuvarsithaVallamsetti <suvarsithavallamsetti@gmail.com>
2025-08-15 11:59:49 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -12,16 +12,17 @@ module tb;
     wire [7:0] uio_out;
     wire [7:0] uio_oe;
 
-    tt_um_unsigned_divider uut (
-        .ui_in(ui_in),
-        .uo_out(uo_out),
-        .uio_in(uio_in),
-        .uio_out(uio_out),
-        .uio_oe(uio_oe),
-        .clk(clk),
-        .rst_n(rst_n),
-        .ena(ena)
-    );
+   tt_um_unsigned_divider uut (
+    .ui_in(ui_in),
+    .uo_out(uo_out),
+    .uio_in(uio_in),
+    .uio_out(uio_out),
+    .uio_oe(uio_oe),
+    .clk(clk),
+    .rst_n(rst_n),
+    .ena(ena)
+);
+
 
     always #5 clk = ~clk;
 
